2d6c30e411ea3004c18d76f8533f371092bbb68a
gabrielCarrenio <carrenocarlosgabriel@gmail.com>
2022-08-04 18:23:52 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import styled from "./css/App.module.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className={styled.container}>
       <Header />
       <Main />
     </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Footer from "./Footer";
+import styled from "../css/Main.module.css";
 
 export default function Main() {
   return (
-    <>
+    <main className={styled.main}>
+      <section className={styled.section}>
+        <h1 className={styled.h1}>
+          Build The Community
+          <br />
+          Your Fans Will Love
+        </h1>
+        <p className={styled.p}>
+          Huddle re-imagines the way we build communities. You have a voice, but
+          so does your audience. Create connections with your users as you
+          engage in genuine discussion.
+        </p>
+        <button className={styled.button}>Register</button>
+      </section>
       <Footer />
-    </>
+    </main>
   );
 }
